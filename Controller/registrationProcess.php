95226926bb2960313a742b7cee5ba32aa78bd32f
Apdil <apdil.aydinalp@gmail.com>
2017-08-03 16:48:34 +0200

    stock user in DB succes

--- Controller/registrationProcess.php
+++ Controller/registrationProcess.php
@@ -2,6 +2,11 @@
 $login = htmlspecialchars($_POST['creatLogin']);
 $mdp = htmlspecialchars($_POST['creatMdp']);
 
-    
+$pdo = new PDO('mysql:host=localhost; dbname=tchat', 'admin', 'pomme');
+$stmt = $pdo->prepare('INSERT INTO `users` (login, mdp) VALUES (:login, :mdp)');
+$stmt->bindValue(':login', $login, PDO::PARAM_STR);
+$stmt->bindValue(':mdp', $mdp, PDO::PARAM_STR);
+$stmt->execute();
 
+header('Location: ../index.html');
 ?>
