1044ee8e5fea0b05baef5b46cac6042c5904ef96
Apdil <apdil.aydinalp@gmail.com>
2017-08-14 18:22:49 +0200

    tchat succes

--- script/getTchatCntnt.js
+++ script/getTchatCntnt.js
@@ -1,33 +1,50 @@
 let displayTchat = document.querySelector('#displayMessg');
+let oldMessag = [];
 
-setInterval(function(){
-    displayTchat.innerHTML = '';
-    fetch('Controller/checkMessages.php').then(function (reponse) {
-        return reponse.json();
-    }).then(function (json) {
-        putMessages(json);
-    }).catch(function (err) {
-        console.log(err.message);
-    })
-    displayTchat.scrollTop = displayTchat.scrollHeight;
-}, 1000);
+let xhr = new XMLHttpRequest();
+
+let intervalID = setInterval(function () {
+xhr.onreadystatechange = function (event) {
+    if (this.readyState === XMLHttpRequest.DONE) {
+        if (this.status === 200) {
+            let reponse = this.responseText;
+            if (reponse != '') { // if database is empty
+                let object = JSON.parse(reponse);
+                if (object !== oldMessag[oldMessag.length - 1]) {
+                    oldMessag.push(object);
+                    displayTchat.innerHTML = "";
+                    for (let commentObject of object) {
+                        putMessages(commentObject); // inner displayTchate display message
+                    }
+                    displayTchat.scrollTop = displayTchat.scrollHeight;
+                }
+            }
+            // document.querySelector('#displayTchat').addEventListener('scroll', function () {
+            //     clearInterval(intervalID);
+            // });
+        } else {
+            console.log("Status de la réponse: %d (%s)", this.status, this.statusText);
+        }
+    }
+};
+xhr.open('GET', 'Controller/checkMessages.php', true);
+xhr.send(null);
+}, 500);
 
 
 function putMessages(object) {
-    for (let message of object) {
-        let div = document.createElement('div');
-        let h5 = document.createElement('h5');
-        let p = document.createElement('p');
-        let date = document.createElement('p');
+    let div = document.createElement('div');
+    let h5 = document.createElement('h5');
+    let p = document.createElement('p');
+    let date = document.createElement('p');
 
-        h5.textContent = message.user;
-        p.textContent = message.comment;
-        date.textContent = message.date;
+    h5.textContent = object.user;
+    p.textContent = object.comment;
+    date.textContent = object.date;
 
-        div.appendChild(h5);
-        div.appendChild(p);
-        div.appendChild(date);
+    div.appendChild(h5);
+    div.appendChild(p);
+    div.appendChild(date);
 
-        displayTchat.appendChild(div);
-    }
+    displayTchat.appendChild(div);
 }
