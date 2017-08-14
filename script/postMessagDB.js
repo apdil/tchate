ad0c7d3102af8afb097d6682de38410391a2a768
Apdil <apdil.aydinalp@gmail.com>
2017-08-14 17:56:01 +0200

    change fetch of xhr

--- script/postMessagDB.js
+++ script/postMessagDB.js
@@ -8,12 +8,31 @@ document.querySelector('#submit').addEventListener('click', function (e) {
     let sec = now.getSeconds();
     now = hh + 'h ' + min + 'm ' + sec + 's';
 
-    fetch('Controller/postMessageDb.php?message='+message+'&date='+now).then(function(reponse){
-        return reponse.text();
-    }).then(function(txt){
-        console.log(txt);
-    }).catch(function(err){
-        console.log(err);
-    });
+    let xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function (event) {
+        // XMLHttpRequest.DONE === 4
+        if (this.readyState === XMLHttpRequest.DONE) {
+            if (this.status === 200) {
+                let object = JSON.parse(this.responseText);
+                console.log(object.login)
+                fetch('Controller/postMessageDb.php?message=' + message + '&date=' + 
+                now+'&nom='+object.login).then(function (reponse) {
+                    return reponse.text();
+                }).then(function (txt) {
+                    console.log(txt);
+                }).catch(function (err) {
+                    console.log(err);
+                });
+
+            } else {
+                console.log("Status de la réponse: %d (%s)", this.status, this.statusText);
+            }
+        }
+    };
+
+    xhr.open('GET', 'Controller/displaySessionUser.php', true);
+    xhr.send(null);
+
 
 });
